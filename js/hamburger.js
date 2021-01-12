f8544603111a840646ff7d9accbe8f98154c84bd
alexandrebatista84 <alexandrebatista84@gmail.com>
2021-01-12 18:21:44 +0000

    UPD: JS

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,6 +5,7 @@ function hamburgerfunction()
 
     var x = document.querySelectorAll('.links');
     var y = document.querySelectorAll('.icons');
+    var z = document.querySelectorAll('#myLinks');
     var i=0;
 
     for(i=0;i<x.length;i++)
@@ -20,12 +21,20 @@ function hamburgerfunction()
 
     for(i=0;i<y.length;i++)
     {
-    if (y[i].style.display === 'none' || y[i].style.display === '' ) {
-        y[i].style.display = 'block';
-    } 
-    else {
-        y[i].style.display = 'none';  
+        if (y[i].style.display === 'none' || y[i].style.display === '' ) {
+            y[i].style.display = 'block';
+        } 
+        else {
+            y[i].style.display = 'none';  
+        }
     }
+
+    for(i=0;i<z.length;i++)
+    {
+    if (z[i].style.display === 'none' || z[i].style.display === '' ) {
+        z[i].style.display = 'block';
+    } 
+    
     
     }
 }
