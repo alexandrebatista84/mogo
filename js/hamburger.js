52b74ceb9b183f92d57fb828c5215156e6beed68
alexandrebatista84 <alexandrebatista84@gmail.com>
2021-01-12 18:15:47 +0000

    UPD:

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,6 +4,7 @@ function hamburgerfunction()
 {
 
     var x = document.querySelectorAll('.links');
+    var y = document.querySelectorAll('.icons');
     var i=0;
 
     for(i=0;i<x.length;i++)
@@ -14,5 +15,17 @@ function hamburgerfunction()
     else {
         x[i].style.display = 'none';  
     }
+    
+    }
+
+    for(i=0;i<y.length;i++)
+    {
+    if (y[i].style.display === 'none' || y[i].style.display === '' ) {
+        y[i].style.display = 'block';
+    } 
+    else {
+        y[i].style.display = 'none';  
+    }
+    
     }
 }
