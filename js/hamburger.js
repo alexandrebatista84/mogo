d507f560df6b88ab1706da4714f446627103c2a9
alexandrebatista84 <alexandrebatista84@gmail.com>
2021-01-13 23:01:35 +0000

    UPD: burger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,38 +1,11 @@
-function hamburgerfunction()
-{
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
 
-    var x = document.querySelectorAll('.menuLink');
-    var y = document.querySelectorAll('.navbar');
-    var z = document.querySelectorAll('.myLinks');
-    var i=0;
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        
+    })
+} 
 
-    for(i=0;i<x.length;i++)
-    {
-    if (x[i].style.display === 'none' || x[i].style.display === '' ) {
-        x[i].style.display = 'block';
-    } 
-    else {
-        x[i].style.display = 'none';  
-    }
-    
-    }
-
-    for(i=0;i<y.length;i++)
-    {
-        if (y[i].style.display === 'none' || y[i].style.display === '' ) {
-            y[i].style.display = 'block';
-        } 
-        else {
-            y[i].style.display = 'none';  
-        }
-    }
-
-    for(i=0;i<z.length;i++)
-    {
-    if (z[i].style.display === 'none' || z[i].style.display === '' ) {
-        z[i].style.display = 'block';
-    } 
-    
-    
-    }
-}
+navSlide();
